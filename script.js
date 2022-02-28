4684748d3907aad07ee652250b9a42f9e12e1d8d
chafin123 <stevend.chafin@gmail.com>
2022-02-28 14:16:43 -0600

    Added typewriter effect and spining button

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const resultsButton = document.querySelector('#dataGetButton');
 const adviceId = document.querySelector('#adviceNumber');
 const apiUrl = "https://api.adviceslip.com/advice";
 
+
 resultsButton.addEventListener("click", () => {
     //Request Data
     fetch(apiUrl, {cache: "no-cache"})
@@ -11,8 +12,20 @@ resultsButton.addEventListener("click", () => {
         let data = response.slip;
         let dataId = data.id;
         let dataAdvice = data.advice;
+        
         //Inject to DOM
         adviceId.innerHTML = `# ${dataId}`;
         resultsQuote.innerHTML = dataAdvice;
+        let text = resultsQuote.innerHTML;
+        resultsQuote.innerHTML = "";
+        let i=0;
+         let timer = setInterval(() => {
+           if(i<text.length){
+             resultsQuote.append(text.charAt(i))
+             i++;
+           } else {
+             clearInterval(timer)
+           }
+         },80)
       });
   });
